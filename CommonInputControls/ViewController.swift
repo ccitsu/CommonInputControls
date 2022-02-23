91a674152aff2259e8abf2358311a09925cc06b9
Shaqra University <ccitlms@gmail.com>
2022-02-23 10:36:41 +0300

    Initial Commit

---
+++ CommonInputControls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by CCIT on 23/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
